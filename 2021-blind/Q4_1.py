0adc47dd1033e5454ab48867c3223d2060fefbd4
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-01-30 17:16:41 +0900

    Create Q4_1.py

---
+++ 2021-blind/Q4_1.py
@@ -0,0 +1,12 @@
+def solution(n, s, a, b, fares):
+    INF = 10**8 + 10
+    d = [[INF]*205 for i in range(205)]
+    for v in fares:
+        d[v[0]][v[1]] = v[2]
+        d[v[1]][v[0]] = v[2]
+    for i in range(1, n+1): d[i][i] = 0
+    for k in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j])    
+    return min(d[s][mid]+d[mid][a]+d[mid][b] for mid in range(1, n+1))
