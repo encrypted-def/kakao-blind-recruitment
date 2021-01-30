12dfac78fb50b781d8b9946c09bcdcb6cbb6a752
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-01-30 23:10:18 +0900

    Update Q5_1.py

--- 2021-blind/Q5_1.py
+++ 2021-blind/Q5_1.py
@@ -13,6 +13,7 @@ def i2s(t):
 
 def solution(play_time, adv_time, logs):
     pt, at = s2i(play_time), s2i(adv_time)
+    # A. 특정 초에 시청중인 사람의 수 계산
     d = [0]*360001
     for l in logs:
         st, en = map(s2i, l.split('-'))
@@ -20,6 +21,7 @@ def solution(play_time, adv_time, logs):
         d[en] -= 1
     for i in range(1, 360001):
         d[i] += d[i-1]
+    # B. 누적 재생시간이 가장 많은 곳 계산
     mxval, mxtime = sum(d[:at]), 0
     curval = mxval
     for i in range(1, 360001-at):
