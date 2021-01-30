c4eb323602f150aaf7582bb25d061a778af2fed8
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-01-30 23:09:25 +0900

    Update Q5_1.cpp

--- 2021-blind/Q5_1.cpp
+++ 2021-blind/Q5_1.cpp
@@ -20,12 +20,14 @@ string i2s(int t){
 
 string solution(string play_time, string adv_time, vector<string> logs) {
     int pt = s2i(play_time), at = s2i(adv_time);
+    // A. 특정 초에 시청중인 사람의 수 계산
     int d[360001] = {};
     for(auto l : logs){
         int st = s2i(l.substr(0, 8)), en = s2i(l.substr(9, 8));
         d[st]++; d[en]--;
     }
     for(int i = 1; i <= 360000; i++) d[i] += d[i-1];
+    // B. 누적 재생시간이 가장 많은 곳 계산
     long long mxval = 0, curval = 0;
     int mxtime = 0;
     for(int i = 0; i < at; i++) curval += d[i];
