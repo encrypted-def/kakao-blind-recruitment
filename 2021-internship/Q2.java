66cbec2f94c08abd1ebe95e9575bd8af34ae35d9
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-07-26 21:35:20 +0900

    Update Q2.java

--- 2021-internship/Q2.java
+++ 2021-internship/Q2.java
@@ -15,11 +15,15 @@ class Solution {
         for(int i = 0; i < 5; i++){
             for(int j = 0; j < 5; j++){
                 if(board[i].charAt(j) != 'P') continue;
+                
+                // 상하좌우로 거리가 1인 응시자 확인
                 for(int dir = 0; dir < 4; dir++){
                     int nx = i+dx1[dir];
                     int ny = j+dy1[dir];
                     if(!OOB(nx,ny) && board[nx].charAt(ny)=='P') return 0;
                 }
+                
+                // 대각선에 위치한 응시자 확인
                 for(int dir = 0; dir < 4; dir++){
                     int nx = i+dx2[dir];
                     int ny = j+dy2[dir];
@@ -27,9 +31,12 @@ class Solution {
                         if(board[i].charAt(ny) != 'X' || board[nx].charAt(j) != 'X') return 0;
                     }
                 }
+                
+                // 상하좌우로 거리가 2인 응시자 확인
                 for(int dir = 0; dir < 4; dir++){
                     int nx = i+2*dx1[dir];
                     int ny = j+2*dy1[dir];
+                    // 거리가 2인 곳에 응시자가 있을 경우 파티션 존재 여부 확인
                     if(!OOB(nx,ny) && board[nx].charAt(ny)=='P'){
                         if(board[i+dx1[dir]].charAt(j+dy1[dir]) != 'X') return 0;
                     }
