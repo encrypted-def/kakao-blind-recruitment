c4ee759d953fd9d4eed2a9f7b2c46f77cdd260d4
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-08-12 22:49:13 +0900

    Update Q3_3.cpp

--- 2021-internship/Q3_3.cpp
+++ 2021-internship/Q3_3.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 string solution(int n, int k, vector<string> cmd) {
     set<int> s;
-    vector<int> erased;
+    stack<int> erased;
     for(int i = 0; i < n; i++)
         s.insert(i);
     auto it = s.find(k);
@@ -17,13 +17,13 @@ string solution(int n, int k, vector<string> cmd) {
             while(num--) it++;
         }
         else if(c[0] == 'C'){
-            erased.push_back(*it);
+            erased.push(*it);
             it = s.erase(it);
             if(it == s.end()) it--;
         }
         else{ // Z
-            s.insert(erased.back());
-            erased.pop_back();
+            s.insert(erased.top());
+            erased.pop();
         }
     }
     string status(n,'X');
