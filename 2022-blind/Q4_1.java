1b38ceac1ec5cf23d4fd6200381e8e41f237cb01
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2022-01-19 23:27:10 +0900

    Update Q4_1.java

--- 2022-blind/Q4_1.java
+++ 2022-blind/Q4_1.java
@@ -45,9 +45,9 @@ class Solution {
         Arrays.fill(mn, -1);
         bt(0, n, info); // backtracking을 통해 ret에 정답을 넣어둠
         if(mn[0] == -1){
-            int[] ret_arr = new int[1];
-            ret_arr[0] = -1;
-            return ret_arr;
+            int[] ret = new int[1];
+            ret[0] = -1;
+            return ret;
         }
         int[] ret = new int[11];
         for(int i = 0; i < 11; i++)
